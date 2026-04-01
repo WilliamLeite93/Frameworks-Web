5c1b0e30434119918d4b0aad33de4c45ae2b1c1c
GABRIEL SILVA ALPOIM AMARO <04642133046@senacrs.edu.br>
2026-04-01 20:52:42 -0300

    Adicionando telas e gerenciando as rotas

---
+++ proj-frameworks/src/pages/Evolution.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Evolução</h1>
+    <p>Gráficos e métricas de progresso.</p>
+  </div>
+</template>
---
+++ proj-frameworks/src/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Página Inicial</h1>
+    <p>Bem-vindo ao sistema.</p>
+    <router-link to="/login">Ir para Login</router-link>
+  </div>
+</template>
